1301c4ee556696d2216a25b8a8d815388525d833
Rasmi Elasmar <rrelasmar@gmail.com>
2015-06-26 08:33:13 -0400

    Updated plots to separate high and low sound speeds.

--- plots/isothermal/isothermal_fluid_high.py
+++ plots/isothermal/isothermal_fluid_high.py
@@ -19,12 +19,13 @@ for simtype in simtypes:
     kvalues_base, ps_base = np.loadtxt(result_dir+base, unpack=True)
 
     for filename in filenames:
-        isothermal = isothermalpattern.findall(filename)[0]
-        n = npattern.findall(filename)[0]
-        kvalues, ps = np.loadtxt(result_dir+filename, unpack=True)
-        ps = (ps - ps_base)/ps_base
-        style = '--' if 'particle' in filename else '-'
-        plt.semilogx(kvalues, ps, style, label=simtype+' n='+n+' cs='+isothermal)
+        if 'isothermal30' in filename or 'isothermal100' in filename:
+            isothermal = isothermalpattern.findall(filename)[0]
+            n = npattern.findall(filename)[0]
+            kvalues, ps = np.loadtxt(result_dir+filename, unpack=True)
+            ps = (ps - ps_base)/ps_base
+            style = '--' if 'particle' in filename else '-'
+            plt.semilogx(kvalues, ps, style, label=simtype+' n='+n+' cs='+isothermal)
 
 # Sort legend numerically by sound speed.
 handles, labels = plt.gca().get_legend_handles_labels()
@@ -34,8 +35,8 @@ handles2, labels2 = zip(*hl)
 plt.xlim([1e-1,1e1])
 plt.xlabel('$k \, (h/Mpc)$', fontsize=14)
 plt.ylabel('$\delta P(k)$', fontsize=14)
-plt.title('Relative Power Spectrum, Isothermal Fluid Comparison at Z=0')
+plt.title('Relative Power Spectrum, Isothermal Fluid Comparison, High Sound Speeds')
 plt.legend(handles2, labels2, numpoints=1, loc='best')
-plt.savefig('isothermal_fluid.png')
+plt.savefig('isothermal_fluid_high.png')
 
 plt.show()
