fbcc10264635959d262df656d3500d940adffc0f
Rasmi Elasmar <rrelasmar@gmail.com>
2015-06-30 08:54:27 -0400

    Added standard fluid and particle data to ps plot. Changed y-axes to log.

--- plots/isothermal/isothermal_fluid_all.py
+++ plots/isothermal/isothermal_fluid_all.py
@@ -9,30 +9,36 @@ results = os.listdir(result_dir)
 isothermalpattern = re.compile(r'_isothermal(\d+)')
 npattern = re.compile(r'_n(\d+)')
 
-simtypes = ['fluid']
+simtypes = ['fluid', 'particle']
 for simtype in simtypes:
-    params = [simtype,'b100','n512','t0.005','h2','isothermal']
+    params = [simtype,'b100','n512','t0.005','h2']
     filenames = [filename for filename in results if all(param in filename for param in params)]
-    base = 'fluid_b100_n512_t0.005_h2_z50.out'
-    
-    kvalues_base, ps_base = np.loadtxt(result_dir+base, unpack=True)
 
     for filename in filenames:
-        isothermal = isothermalpattern.findall(filename)[0]
         n = npattern.findall(filename)[0]
+        label = simtype+' n='+n
+        if 'isothermal' in filename:
+            isothermal = isothermalpattern.findall(filename)[0]
+            label += ' cs='+isothermal
         kvalues, ps = np.loadtxt(result_dir+filename, unpack=True)
-        style = '--' if 'particle' in filename else '-'
-        plt.semilogx(kvalues, ps, style, label=simtype+' n='+n+' cs='+isothermal)
+        style = '--' if 'isothermal' in filename else '-'
+        plt.loglog(kvalues, ps, style, label=label)
 
 # Sort legend numerically by sound speed.
+def sortlabels(item):
+    if len(item[1].split(' ')) > 2:
+        return int(item[1].split(' ')[2].split('=')[1])
+    else:
+        return item[1].split(' ')[0]
+
 handles, labels = plt.gca().get_legend_handles_labels()
-hl = sorted(zip(handles, labels), key=lambda item:  int(item[1].split(' ')[2].split('=')[1]))
+hl = sorted(zip(handles, labels), key=lambda item: sortlabels(item))
 handles2, labels2 = zip(*hl)
 
 plt.xlim([1e-1,1e1])
 plt.xlabel('$k \, (h/Mpc)$', fontsize=14)
 plt.ylabel('$P(k)$', fontsize=14)
-plt.title('Power Spectrum, Isothermal Fluid Comparison at Z=0')
+plt.title('Power Spectrum, Fluid and Particle Comparison at Z=0')
 plt.legend(handles2, labels2, numpoints=1, loc='best')
 plt.savefig('isothermal_fluid_all.png')
 
