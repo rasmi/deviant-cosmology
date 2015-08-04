f462abf84110391ca4aeca9d044d8688945b909e
Rasmi Elasmar <rrelasmar@gmail.com>
2015-08-04 09:18:12 -0400

    Added parameter files, extended plots out to z=5.

--- plots/isothermal/isothermal_fluid_low_adiabatic_normalized_particle.py
+++ plots/isothermal/isothermal_fluid_low_adiabatic_normalized_particle.py
@@ -31,8 +31,8 @@ for simtype in simtypes:
             ps = (ps - ps_base)/ps_base
             plt.semilogx(kvalues, ps, '--', label='Adiabatic fluid')
 
-plt.xlim([1e-1,1e0])
-plt.ylim([-0.1,0.1])
+plt.xlim([1e-1,5e0])
+plt.ylim([-0.1,2])
 plt.xlabel('$k \, (h/Mpc)$', fontsize=14)
 plt.ylabel('$\delta P(k)$', fontsize=14)
 plt.title('Relative Power Spectrum, Isothermal and Adiabatic Fluid Comparison')
--- plots/isothermal/isothermal_fluid_low_adiabatic_normalized_particle_z10.py
+++ plots/isothermal/isothermal_fluid_low_adiabatic_normalized_particle_z10.py
@@ -31,7 +31,7 @@ for simtype in simtypes:
             ps = (ps - ps_base)/ps_base
             plt.semilogx(kvalues, ps, '--', label='Adiabatic fluid')
 
-plt.xlim([1e-1,1e0])
+plt.xlim([1e-1,5e0])
 plt.ylim([-0.1,0.1])
 plt.xlabel('$k \, (h/Mpc)$', fontsize=14)
 plt.ylabel('$\delta P(k)$', fontsize=14)
