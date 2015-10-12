1aa5953061c18f1b50d675b2bd8b2816b41f35c9
Rasmi Elasmar <rrelasmar@gmail.com>
2015-10-12 19:36:49 -0400

    Added n=512 test for cs=100, b=200 to test resolution effects. Updated plot.

--- plots/isothermal/isothermal_fluid_low_adiabatic_normalized_particle_b200_resolution.py
+++ plots/isothermal/isothermal_fluid_low_adiabatic_normalized_particle_b200_resolution.py
@@ -14,6 +14,7 @@ for simtype in simtypes:
     params = [simtype,'b200','n1024','t0.005','h2','isothermal']
     filenames = [filename for filename in results if all(param in filename for param in params)]
     filenames.append('fluid_b200_n512_t0.005_h2_isothermal10.out')
+    filenames.append('fluid_b200_n512_t0.005_h2_isothermal100.out')
     filenames.append('fluid_b200_n1024_t0.005_h2_z50.out')
     base = 'particle_b200_n1024_t0.005_h2_z50.out'
     
@@ -22,7 +23,7 @@ for simtype in simtypes:
     for filename in filenames:
         if 'isothermal' in filename:
             isothermal = isothermalpattern.findall(filename)[0]
-            if int(isothermal)  == 10:
+            if int(isothermal)  in [10,100]:
                 n = npattern.findall(filename)[0]
                 kvalues, ps = np.loadtxt(result_dir+filename, unpack=True)
                 ps = (ps - ps_base)/ps_base
@@ -33,7 +34,7 @@ for simtype in simtypes:
             plt.semilogx(kvalues, ps, '--', label='Adiabatic fluid')
 
 plt.xlim([0.05,5e0])
-plt.ylim([-0.3,0.3])
+plt.ylim([-0.35,0.3])
 plt.xlabel('$k \, (h/Mpc)$', fontsize=14)
 plt.ylabel('$\delta P(k)$', fontsize=14)
 plt.title('Relative Power Spectrum, Isothermal and Adiabatic Fluid Comparison 200Mpc/h')
