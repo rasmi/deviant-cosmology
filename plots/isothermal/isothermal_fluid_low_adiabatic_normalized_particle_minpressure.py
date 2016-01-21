e688ddf197247a1e7d7c67fa13c6414426109aaa
Rasmi Elasmar <rrelasmar@gmail.com>
2016-01-21 00:00:05 -0500

    Added linear particle predictions for zstart=100 results.

--- plots/isothermal/isothermal_fluid_low_adiabatic_normalized_particle_minpressure.py
+++ plots/isothermal/isothermal_fluid_low_adiabatic_normalized_particle_minpressure.py
@@ -9,14 +9,14 @@ results = os.listdir(result_dir)
 isothermalpattern = re.compile(r'_isothermal(\d+)')
 npattern = re.compile(r'_n(\d+)')
 
+base = 'particle_b100_n1024_t0.005_h2_z100.out'
+kvalues_base, ps_base = np.loadtxt(result_dir+base, unpack=True)
+
 simtypes = ['fluid']
 for simtype in simtypes:
     params = [simtype,'b100','n1024','t0.005','h2','isothermal', 'z100']
     filenames = [filename for filename in results if all(param in filename for param in params)]
     filenames.append('fluid_b100_n1024_t0.005_h2_z100.out')
-    base = 'particle_b100_n1024_t0.005_h2_z100.out'
-    
-    kvalues_base, ps_base = np.loadtxt(result_dir+base, unpack=True)
 
     for filename in filenames:
         if 'isothermal' in filename and 'minpressure16' in filename:
@@ -32,8 +32,13 @@ for simtype in simtypes:
             ps = (ps - ps_base)/ps_base
             plt.semilogx(kvalues, ps, '--', label='Adiabatic fluid')
 
+linear_kvalues, linear_ps = np.loadtxt(result_dir+'particle_b100_n1024_t0.005_h2_z100_z100.out', unpack=True)
+linear_ps*=5983.9
+linear_ps = (linear_ps - ps_base)/ps_base
+plt.semilogx(linear_kvalues, linear_ps, '-', label='Predicted linear particle')
+
 plt.xlim([1e-1,5e0])
-plt.ylim([-0.1,0.5])
+plt.ylim([-1.0,0.5])
 plt.xlabel('$k \, (h/Mpc)$', fontsize=14)
 plt.ylabel('$\delta P(k)$', fontsize=14)
 plt.title('Relative Power Spectrum, Isothermal and Adiabatic Fluid Comparison')
