57cf0f73186b48503f79df6a72a75e8ec4838544
Rasmi Elasmar <rrelasmar@gmail.com>
2016-02-05 11:02:05 -0500

    Updated z=10 results with cs=200 and cs=50 analytic predictions.

--- plots/isothermal/isothermal_fluid_low_adiabatic_normalized_particle_z10_minpressure.py
+++ plots/isothermal/isothermal_fluid_low_adiabatic_normalized_particle_z10_minpressure.py
@@ -10,6 +10,8 @@ isothermalpattern = re.compile(r'_iso(\d+)')
 testgrowthpattern = re.compile(r'testgrowthcs(\d+)')
 npattern = re.compile(r'_n(\d+)')
 
+soundspeeds = ['10','30','50','100','200','300']
+
 simtypes = ['fluid']
 for simtype in simtypes:
     params = [simtype,'b100','n1024','t0.005','h2','iso','z10_','z100']
@@ -22,7 +24,7 @@ for simtype in simtypes:
     for filename in filenames:
         if 'iso' in filename and 'minpressure16' in filename:
             isothermal = isothermalpattern.findall(filename)[0]
-            if isothermal in ['10','30','50','100','200','300']:
+            if isothermal in soundspeeds:
                 n = npattern.findall(filename)[0]
                 kvalues, ps = np.loadtxt(result_dir+filename, unpack=True)
                 ps = (ps - ps_base)/ps_base
@@ -35,7 +37,7 @@ for simtype in simtypes:
 filenames = [filename for filename in results if 'testgrowth' in filename]
 for filename in filenames:
     testgrowth = testgrowthpattern.findall(filename)[0]
-    if testgrowth in ['10','30','100','300']:
+    if testgrowth in soundspeeds:
         label = 'predicted cs='+testgrowth
         col1, kvalues, linearpower, col4, col5, sqrtsuppression = np.loadtxt(result_dir+filename, unpack=True)
         style = '-x'
@@ -55,7 +57,7 @@ plt.ylim([-0.3,0.1])
 plt.xlabel('$k \, (h/Mpc)$', fontsize=14)
 plt.ylabel('$\delta P(k)$', fontsize=14)
 plt.title('Relative Power Spectrum, Isothermal and Adiabatic Fluid Comparison z=10')
-plt.legend(numpoints=1, loc='best', fontsize=11)
+plt.legend(numpoints=1, loc='best', fontsize=10)
 plt.savefig('isothermal_fluid_low_adiabatic_normalized_particle_z10_minpressure.png')
 
 plt.show()
