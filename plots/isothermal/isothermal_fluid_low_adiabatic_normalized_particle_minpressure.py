ba07aa2325002205054c48dd9779ccd9729fb00f
Rasmi Elasmar <rrelasmar@gmail.com>
2016-02-25 13:05:52 -0500

    Adding larger box sizes.

--- plots/isothermal/isothermal_fluid_low_adiabatic_normalized_particle_minpressure.py
+++ plots/isothermal/isothermal_fluid_low_adiabatic_normalized_particle_minpressure.py
@@ -11,6 +11,9 @@ npattern = re.compile(r'_n(\d+)')
 
 base = 'particle_b100_n1024_t0.005_h2_z100.out'
 kvalues_base, ps_base = np.loadtxt(result_dir+base, unpack=True)
+outofrange = len(kvalues_base) - len(kvalues_base[kvalues_base > 0.2])
+kvalues_base = kvalues_base[kvalues_base > 0.2]
+ps_base = ps_base[outofrange:]
 
 simtypes = ['fluid']
 for simtype in simtypes:
@@ -24,19 +27,60 @@ for simtype in simtypes:
             if int(isothermal) <= 300:
                 n = npattern.findall(filename)[0]
                 kvalues, ps = np.loadtxt(result_dir+filename, unpack=True)
+                kvalues = kvalues[outofrange:]
+                ps = ps[outofrange:]
                 ps = (ps - ps_base)/ps_base
                 style = '-'
                 plt.semilogx(kvalues, ps, style, label='Isothermal fluid cs='+isothermal)
         elif filename is 'fluid_b100_n1024_t0.005_h2_z100.out': 
             kvalues, ps = np.loadtxt(result_dir+filename, unpack=True)
+            kvalues = kvalues[outofrange:]
+            ps = ps[outofrange:]
             ps = (ps - ps_base)/ps_base
             plt.semilogx(kvalues, ps, '--', label='Adiabatic fluid')
+
+base = 'particle_b400_n1024_t0.005_h2_z100.out'
+kvalues_base, ps_base = np.loadtxt(result_dir+base, unpack=True)
+kvalues_base = kvalues_base[kvalues_base < 0.4]
+ps_base = ps_base[:len(kvalues_base)]
+
+simtypes = ['fluid']
+for simtype in simtypes:
+    params = [simtype,'b400','n1024','t0.005','h2','isothermal', 'z100']
+    filenames = [filename for filename in results if all(param in filename for param in params)]
+    filenames.append('fluid_b400_n1024_t0.005_h2_z100.out')
+
+    for filename in filenames:
+        if 'isothermal' in filename and 'minpressure16' in filename:
+            isothermal = isothermalpattern.findall(filename)[0]
+            print isothermal
+            if int(isothermal) <= 300:
+                n = npattern.findall(filename)[0]
+                kvalues, ps = np.loadtxt(result_dir+filename, unpack=True)
+                kvalues = kvalues[:len(kvalues_base)]
+                ps = ps[:len(kvalues)]
+                ps = (ps - ps_base)/ps_base
+                style = '-'
+                plt.semilogx(kvalues, ps, style, label=None)
+        elif filename is 'fluid_b400_n1024_t0.005_h2_z100.out': 
+            kvalues, ps = np.loadtxt(result_dir+filename, unpack=True)
+            kvalues = kvalues[:len(kvalues_base)]
+            ps = ps[:len(kvalues)]
+            ps = (ps - ps_base)/ps_base
+            plt.semilogx(kvalues, ps, '--', label=None)
 
 _, linear_kvalues, linear_ps, nonlinear_ps = np.loadtxt(result_dir+'testPk0.dat', unpack=True)
 linear_ps = (linear_ps - nonlinear_ps)/nonlinear_ps
 plt.semilogx(linear_kvalues, linear_ps, '-x', label='Predicted linear particle')
 
-plt.xlim([0.1,1])
+_, linear_kvalues_400, _, nonlinear_ps_400, linear_ps_400, oneloop_400 = np.loadtxt(result_dir+'testPk0.400.dat', unpack=True)
+linear_ps_400 = (linear_ps_400 - nonlinear_ps_400)/nonlinear_ps_400
+oneloop_400 = (oneloop_400 - nonlinear_ps_400)/nonlinear_ps_400
+plt.semilogx(linear_kvalues_400, linear_ps_400, '-x', label='linear prediction')
+plt.semilogx(linear_kvalues_400, oneloop_400, '-x', label='one-loop')
+
+
+plt.xlim([0.027,1])
 plt.ylim([-1.0,0.3])
 plt.xlabel('$k \, (h/Mpc)$', fontsize=14)
 plt.ylabel('$\delta P(k)$', fontsize=14)
--- plots/isothermal/isothermal_fluid_low_adiabatic_normalized_particle_z10_minpressure_z100.py
+++ plots/isothermal/isothermal_fluid_low_adiabatic_normalized_particle_z10_minpressure_z100.py
@@ -10,21 +10,19 @@ isothermalpattern = re.compile(r'_iso(\d+)')
 testgrowthpattern = re.compile(r'testgrowthcs(\d+)')
 npattern = re.compile(r'_n(\d+)')
 
-soundspeeds = ['10','30','50','100','200','300']
-
 simtypes = ['fluid']
 for simtype in simtypes:
-    params = [simtype,'b100','n1024','t0.005','h2','iso','z10_','z100']
+    params = [simtype,'b100','n1024','t0.005','h2','iso','z10_']
     filenames = [filename for filename in results if all(param in filename for param in params)]
     filenames.append('fluid_b100_n1024_t0.005_h2_z100_z10.out')
-
-    base = 'particle_b100_n1024_t0.005_h2_z100_z10.out'
+    base = 'particle_b100_n1024_t0.005_h2_z10.out'
+    
     kvalues_base, ps_base = np.loadtxt(result_dir+base, unpack=True)
 
     for filename in filenames:
         if 'iso' in filename and 'minpressure16' in filename:
             isothermal = isothermalpattern.findall(filename)[0]
-            if isothermal in soundspeeds:
+            if isothermal in ['10','30','100']:
                 n = npattern.findall(filename)[0]
                 kvalues, ps = np.loadtxt(result_dir+filename, unpack=True)
                 ps = (ps - ps_base)/ps_base
@@ -34,53 +32,23 @@ for simtype in simtypes:
             ps = (ps - ps_base)/ps_base
             plt.semilogx(kvalues, ps, '--', label='Adiabatic fluid')
 
-base = 'particle_b400_n1024_t0.005_h2_z100_z10.out'
-kvalues_base, ps_base = np.loadtxt(result_dir+base, unpack=True)
-kvalues_base = kvalues_base[kvalues_base < 10]
-ps_base = ps_base[:len(kvalues_base)]
-
-simtypes = ['fluid']
-for simtype in simtypes:
-    params = [simtype,'b400','n1024','t0.005','h2','iso','z10_','z100']
-    filenames = [filename for filename in results if all(param in filename for param in params)]
-    filenames.append('fluid_b400_n1024_t0.005_h2_z100_z10.out')
-    print filenames
-    for filename in filenames:
-        if 'iso' in filename and 'minpressure16' in filename:
-            isothermal = isothermalpattern.findall(filename)[0]
-            print isothermal
-            if int(isothermal) <= 300:
-                n = npattern.findall(filename)[0]
-                kvalues, ps = np.loadtxt(result_dir+filename, unpack=True)
-                kvalues = kvalues[:len(kvalues_base)]
-                ps = ps[:len(kvalues)]
-                ps = (ps - ps_base)/ps_base
-                style = '-'
-                plt.semilogx(kvalues, ps, style, label=None)
-        elif filename is 'fluid_b400_n1024_t0.005_h2_z100_z10.out': 
-            kvalues, ps = np.loadtxt(result_dir+filename, unpack=True)
-            kvalues = kvalues[:len(kvalues_base)]
-            ps = ps[:len(kvalues)]
-            ps = (ps - ps_base)/ps_base
-            plt.semilogx(kvalues, ps, '--', label=None)
-
 filenames = [filename for filename in results if 'testgrowth' in filename]
 for filename in filenames:
     testgrowth = testgrowthpattern.findall(filename)[0]
-    if testgrowth in soundspeeds:
+    if testgrowth in ['10','30','100']:
         label = 'predicted cs='+testgrowth
         col1, kvalues, linearpower, col4, col5, sqrtsuppression = np.loadtxt(result_dir+filename, unpack=True)
         style = '-x'
         ps = (sqrtsuppression**2) - 1
         #ps /= 4*np.pi**2
-        #plt.semilogx(kvalues, ps, style, label=label)
+        plt.semilogx(kvalues, ps, style, label=label)
 
-plt.xlim([0.1,7.0])
-plt.ylim([-0.3,0.1])
+plt.xlim([1e-1,5e0])
+plt.ylim([-0.1,0.1])
 plt.xlabel('$k \, (h/Mpc)$', fontsize=14)
 plt.ylabel('$\delta P(k)$', fontsize=14)
 plt.title('Relative Power Spectrum, Isothermal and Adiabatic Fluid Comparison z=10')
-plt.legend(numpoints=1, loc='best', fontsize=10)
+plt.legend(numpoints=1, loc='best')
 plt.savefig('isothermal_fluid_low_adiabatic_normalized_particle_z10_minpressure.png')
 
 plt.show()
