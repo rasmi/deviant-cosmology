3c1bc10f04cfc49eef5a1ba0eeff45315720b6c4
Rasmi Elasmar <rrelasmar@gmail.com>
2016-07-13 14:50:06 -0400

    Using scipy.ndimage.filters.gaussian_filter instead of convolve.

--- scripts/convolve.py
+++ scripts/convolve.py
@@ -10,7 +10,7 @@ directory = args.directory
 
 import numpy as np
 import h5py
-from scipy.signal import convolve
+from scipy.ndimage.filters import gaussian_filter
 import yt
 
 fields = {
@@ -18,39 +18,23 @@ fields = {
     'pressure': ('gas', 'pressure')
 }
 
-def gaussian3D(sigma, start, stop, n):
-    xaxis = np.linspace(start, stop, n)
-    yaxis = np.linspace(start, stop, n)
-    zaxis = np.linspace(start, stop, n)
-    x = xaxis[:,None,None]
-    y = yaxis[None,:,None]
-    z = zaxis[None,None,:]
-
-    sigma_square = sigma**2
-
-    return np.exp(-(x**2 + y**2 + z**2)/(2.0*sigma_square))/(np.sqrt(2.0*np.pi*sigma_square))
-
 ds = yt.load(directory+'/'+directory)
 ad = ds.all_data()
 
-resolution = ds.domain_dimensions[0]
-
 sigma = 2.0/3.0
-gaussian = gaussian3D(sigma, -100, 100, resolution)
 
 density = ad[fields['density']]
 pressure = ad[fields['pressure']]
 
-density = density.reshape(gaussian.shape)
-pressure = pressure.reshape(gaussian.shape)
+density = density.reshape(ds.domain_dimensions)
+pressure = pressure.reshape(ds.domain_dimensions)
 
-density_convolution = convolve(density, gaussian, mode='same')
-pressure_convolution = convolve(pressure, gaussian, mode='same')
+density_convolution = gaussian_filter(density, sigma=sigma)
+pressure_convolution = gaussian_filter(pressure, sigma=sigma)
 
-convolution_file = h5py.File('convolution_%s.hdf5' % directory, 'w')
+convolution_file = h5py.File('convolution_gaussian_%s.hdf5' % directory, 'w')
 
 convolution_file.create_dataset('density', data=density_convolution)
 convolution_file.create_dataset('pressure', data=pressure_convolution)
-convolution_file.create_dataset('gaussian', data=gaussian)
 
 convolution_file.close()
