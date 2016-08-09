92874d7827e56e69f004643fbb71970586e0fbcd
Rasmi Elasmar <rrelasmar@gmail.com>
2016-08-09 14:54:14 -0400

    Generalized convolution process, added velocity fields.

--- scripts/convolve.py
+++ scripts/convolve.py
@@ -13,9 +13,14 @@ import h5py
 from scipy.ndimage.filters import gaussian_filter
 import yt
 
+convolution_file = h5py.File('convolution_%s.hdf5' % directory, 'w')
+
 fields = {
     'density': ('gas', 'density'),
-    'pressure': ('gas', 'pressure')
+    'pressure': ('gas', 'pressure'),
+    'velocity_x': ('gas', 'velocity_x'),
+    'velocity_y': ('gas', 'velocity_y'),
+    'velocity_z': ('gas', 'velocity_z')
 }
 
 ds = yt.load(directory+'/'+directory)
@@ -23,15 +28,9 @@ ad = ds.covering_grid(level=0, left_edge=[0.0, 0.0, 0.0], dims=ds.domain_dimensi
 
 sigma = 2.0/3.0
 
-density = ad[fields['density']]
-pressure = ad[fields['pressure']]
-
-density_convolution = gaussian_filter(density, sigma=sigma)
-pressure_convolution = gaussian_filter(pressure, sigma=sigma)
-
-convolution_file = h5py.File('convolution_%s.hdf5' % directory, 'w')
-
-convolution_file.create_dataset('density', data=density_convolution)
-convolution_file.create_dataset('pressure', data=pressure_convolution)
+for fieldname, fieldvalue in fields.iteritems():
+    field = ad[fieldvalue]
+    convolution = gaussian_filter(field, sigma=sigma)
+    convolution_file.create_dataset(fieldname, data=convolution)
 
 convolution_file.close()
