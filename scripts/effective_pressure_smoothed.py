0e4a4ccff257d453b25e6fb887e965c32869b17f
Rasmi Elasmar <rrelasmar@gmail.com>
2016-08-10 02:30:33 -0400

    Added effective pressure calculation for smoothed field. Cleaned up scripts.

--- scripts/effective_pressure_smoothed.py
+++ scripts/effective_pressure_smoothed.py
@@ -14,14 +14,27 @@ import h5py
 
 fields = {
     'density': ('gas', 'density'),
-    'pressure': ('gas', 'pressure'),
     'velocity_x': ('gas', 'velocity_x'),
     'velocity_y': ('gas', 'velocity_y'),
     'velocity_z': ('gas', 'velocity_z')
 }
 
 ds = yt.load(directory+'/'+directory)
-ad = ds.all_data()
+
+convolution_file = h5py.File('convolution_%s.hdf5' % directory, 'r')
+
+convolution_data = {}
+
+for fieldname, fieldvalue in fields.iteritems():
+    field = np.array(convolution_file[fieldname][:])
+    convolution_data[fieldvalue] = field
+
+bbox = np.array([[-1.0, 1.0], [-1.0, 1.0], [-1.0, 1.0]])
+data_shape = convolution_ds.values()[0].shape
+
+convolution_ds = yt.load_uniform_grid(convolution_data, data_shape, length_unit=ds.length_unit, bbox=bbox)
+
+ad = convolution_ds.all_data()
 
 density = ad[fields['density']]
 
@@ -41,7 +54,7 @@ p_eff_x = compute_effective_pressure(velocity_x)
 p_eff_y = compute_effective_pressure(velocity_y)
 p_eff_z = compute_effective_pressure(velocity_z)
 
-pressure_file = h5py.File('effective_pressure_%s.hdf5' % directory, 'w')
+pressure_file = h5py.File('effective_pressure_smoothed_%s.hdf5' % directory, 'w')
 pressure_file.create_dataset('p_eff_x', data=p_eff_x)
 pressure_file.create_dataset('p_eff_y', data=p_eff_y)
 pressure_file.create_dataset('p_eff_z', data=p_eff_z)
@@ -49,7 +62,7 @@ pressure_file.close()
 
 subset = np.random.choice(len(p_eff_x), 1000)
 
-pressure_subset_file = h5py.File('effective_pressure_subset_%s.hdf5' % directory, 'w')
+pressure_subset_file = h5py.File('effective_pressure_smoothed_subset_%s.hdf5' % directory, 'w')
 pressure_subset_file.create_dataset('p_eff_x', data=p_eff_x[subset])
 pressure_subset_file.create_dataset('p_eff_y', data=p_eff_y[subset])
 pressure_subset_file.create_dataset('p_eff_z', data=p_eff_z[subset])
