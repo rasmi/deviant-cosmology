b89653f232d257edca91167361d6adeecdc77ebc
Rasmi Elasmar <rrelasmar@gmail.com>
2016-08-19 02:10:49 -0400

    Doing diffs in 3D instead of with flattened data.

--- scripts/effective_pressure.py
+++ scripts/effective_pressure.py
@@ -21,7 +21,7 @@ fields = {
 }
 
 ds = yt.load(directory+'/'+directory)
-ad = ds.all_data()
+ad = ds.covering_grid(level=0, left_edge=[0.0, 0.0, 0.0], dims=ds.domain_dimensions)
 
 density = ad[fields['density']]
 
@@ -29,17 +29,17 @@ velocity_x = ad[fields['velocity_x']]
 velocity_y = ad[fields['velocity_y']]
 velocity_z = ad[fields['velocity_z']]
 
-def compute_effective_pressure(velocity):
-    dv = np.diff(velocity)
+def compute_effective_pressure(velocity, axis, density_slice):
+    dv = np.diff(velocity, axis=axis)
     dv[dv > 0] = 0
     dv = np.square(dv)
-    dv *= 2*density[:-1]
+    dv *= 2*density_slice
 
     return dv
 
-p_eff_x = compute_effective_pressure(velocity_x)
-p_eff_y = compute_effective_pressure(velocity_y)
-p_eff_z = compute_effective_pressure(velocity_z)
+p_eff_x = compute_effective_pressure(velocity_x, 0, density[:-1])
+p_eff_y = compute_effective_pressure(velocity_y, 1, density[:,:-1])
+p_eff_z = compute_effective_pressure(velocity_z, 2, density[:,:,:-1])
 
 pressure_file = h5py.File('effective_pressure_%s.hdf5' % directory, 'w')
 pressure_file.create_dataset('p_eff_x', data=p_eff_x)
@@ -50,9 +50,9 @@ pressure_file.close()
 subset = np.random.choice(len(p_eff_x), 1000)
 
 pressure_subset_file = h5py.File('effective_pressure_subset_%s.hdf5' % directory, 'w')
-pressure_subset_file.create_dataset('p_eff_x', data=p_eff_x[subset])
-pressure_subset_file.create_dataset('p_eff_y', data=p_eff_y[subset])
-pressure_subset_file.create_dataset('p_eff_z', data=p_eff_z[subset])
-pressure_subset_file.create_dataset('density', data=density[:-1][subset])
+pressure_subset_file.create_dataset('p_eff_x', data=p_eff_x.flatten()[subset])
+pressure_subset_file.create_dataset('p_eff_y', data=p_eff_y.flatten()[subset])
+pressure_subset_file.create_dataset('p_eff_z', data=p_eff_z.flatten()[subset])
+pressure_subset_file.create_dataset('density', data=density.flatten()[subset])
 pressure_subset_file.create_dataset('subset', data=subset)
 pressure_subset_file.close()
--- scripts/effective_pressure_smoothed.py
+++ scripts/effective_pressure_smoothed.py
@@ -34,7 +34,7 @@ data_shape = convolution_data.values()[0].shape
 
 convolution_ds = yt.load_uniform_grid(convolution_data, data_shape, length_unit=ds.length_unit, bbox=bbox)
 
-ad = convolution_ds.all_data()
+ad = convolution_ds.covering_grid(level=0, left_edge=[0.0, 0.0, 0.0], dims=ds.domain_dimensions)
 
 density = ad[fields['density']]
 
@@ -42,17 +42,17 @@ velocity_x = ad[fields['velocity_x']]
 velocity_y = ad[fields['velocity_y']]
 velocity_z = ad[fields['velocity_z']]
 
-def compute_effective_pressure(velocity):
-    dv = np.diff(velocity)
+def compute_effective_pressure(velocity, axis, density_slice):
+    dv = np.diff(velocity, axis=axis)
     dv[dv > 0] = 0
     dv = np.square(dv)
-    dv *= 2*density[:-1]
+    dv *= 2*density_slice
 
     return dv
 
-p_eff_x = compute_effective_pressure(velocity_x)
-p_eff_y = compute_effective_pressure(velocity_y)
-p_eff_z = compute_effective_pressure(velocity_z)
+p_eff_x = compute_effective_pressure(velocity_x, 0, density[:-1])
+p_eff_y = compute_effective_pressure(velocity_y, 1, density[:,:-1])
+p_eff_z = compute_effective_pressure(velocity_z, 2, density[:,:,:-1])
 
 pressure_file = h5py.File('effective_pressure_smoothed_%s.hdf5' % directory, 'w')
 pressure_file.create_dataset('p_eff_x', data=p_eff_x)
@@ -63,9 +63,9 @@ pressure_file.close()
 subset = np.random.choice(len(p_eff_x), 1000)
 
 pressure_subset_file = h5py.File('effective_pressure_smoothed_subset_%s.hdf5' % directory, 'w')
-pressure_subset_file.create_dataset('p_eff_x', data=p_eff_x[subset])
-pressure_subset_file.create_dataset('p_eff_y', data=p_eff_y[subset])
-pressure_subset_file.create_dataset('p_eff_z', data=p_eff_z[subset])
-pressure_subset_file.create_dataset('density', data=density[:-1][subset])
+pressure_subset_file.create_dataset('p_eff_x', data=p_eff_x.flatten()[subset])
+pressure_subset_file.create_dataset('p_eff_y', data=p_eff_y.flatten()[subset])
+pressure_subset_file.create_dataset('p_eff_z', data=p_eff_z.flatten()[subset])
+pressure_subset_file.create_dataset('density', data=density.flatten()[subset])
 pressure_subset_file.create_dataset('subset', data=subset)
 pressure_subset_file.close()
