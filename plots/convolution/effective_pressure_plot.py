f19987fbf4b2002c26dc80ebc884cba988561f88
Rasmi Elasmar <rrelasmar@gmail.com>
2016-10-23 23:38:08 -0400

    Switched pressure-density axes and fitted pressure-density line slopes for effective pressure.

--- plots/convolution/effective_pressure_plot.py
+++ plots/convolution/effective_pressure_plot.py
@@ -16,8 +16,22 @@ def plot_effective_pressure(pressuretype):
     pressure = np.array(effective_pressure_file['pressure'][:])
     norm = np.sqrt(np.square(p_eff_x) + np.square(p_eff_y) + np.square(p_eff_z))
     soundspeed = np.sqrt((5.0/3.0) * (pressure / density))
+    slope, intercept = np.polyfit(density, pressure, 1)
+    soundspeed_eff = np.sqrt((5.0/3.0) * (norm / density))
+    slope_eff, intercept_eff = np.polyfit(density, norm, 1)
+    
     print pressuretype
-    print '%.4g' % np.mean(soundspeed)
+
+    print '--------- Pressure ---------'
+    print 'Mean %.4g' % np.mean(soundspeed)
+    print 'Slope %.4g' % slope
+    print 'Intercept %.4g' % intercept
+
+    print '---- Effective Pressure ----'
+    print 'Mean %.4g' % np.mean(soundspeed_eff)
+    print 'Slope %.4g' % slope_eff
+    print 'Intercept %.4g' % intercept_eff
+    print '----------------------------'
 
     effective_pressure = pd.DataFrame(
         {'p_eff_x': p_eff_x,
@@ -34,7 +48,7 @@ def plot_effective_pressure(pressuretype):
     effective_pressure.plot.scatter(x='density', y='p_eff_y', loglog=True, ylim=[10e-25,10e-14], figsize=(20, 15), fontsize=20).get_figure().savefig('p_eff_y%s.png' % pressuretype)
     effective_pressure.plot.scatter(x='density', y='p_eff_z', loglog=True, ylim=[10e-25,10e-14], figsize=(20, 15), fontsize=20).get_figure().savefig('p_eff_z%s.png' % pressuretype)
     effective_pressure.plot.scatter(x='density', y='norm', loglog=True, ylim=[10e-25,10e-14], figsize=(20, 15), fontsize=20).get_figure().savefig('p_eff_norm%s.png' % pressuretype)
-    effective_pressure.plot.scatter(x='pressure', y='density', loglog=True, figsize=(20, 15), fontsize=20).get_figure().savefig('pressure_density%s.png' % pressuretype)
+    effective_pressure.plot.scatter(x='density', y='pressure', loglog=True, figsize=(20, 15), fontsize=20).get_figure().savefig('pressure_density%s.png' % pressuretype)
     plt.clf()
     effective_pressure['soundspeed'].hist().get_figure().savefig('soundspeed%s.png' % pressuretype)
 
