c5692f3dfff1830ba258e55f50d1d0535d9584f0
Rasmi Elasmar <rrelasmar@gmail.com>
2017-09-10 17:41:55 -0400

    Getting density and pressure from original data.

--- scripts/plot_hist2d.py
+++ scripts/plot_hist2d.py
@@ -2,17 +2,28 @@
 import matplotlib
 matplotlib.use('Agg')
 import numpy as np
+import yt
 import matplotlib.pyplot as plt
 from matplotlib.colors import LogNorm
 import h5py
+
+fields = {
+    'density': ('gas', 'density'),
+    'pressure': ('gas', 'pressure'),
+}
+
+ds = yt.load('RD0000/RD0000')
+ad = ds.covering_grid(level=0, left_edge=[0.0, 0.0, 0.0], dims=ds.domain_dimensions)
+
+density = np.array(ad[fields['density']][:])
+pressure = np.array(ad[fields['pressure']][:])
 
 filename = 'effective_pressure_RD0000.hdf5'
 effective_pressure_file = h5py.File(filename)
 p_eff_x = np.array(effective_pressure_file['p_eff_x'][:])
 p_eff_y = np.array(effective_pressure_file['p_eff_y'][:])
 p_eff_z = np.array(effective_pressure_file['p_eff_z'][:])
-density = np.array(effective_pressure_file['density'][:])
-pressure = np.array(effective_pressure_file['pressure'][:])
+
 norm = np.sqrt(np.square(p_eff_x) + np.square(p_eff_y) + np.square(p_eff_z))
 soundspeed = np.sqrt((pressure / density)) / 1e5
 soundspeed_eff = np.sqrt((norm / density)) / 1e5
